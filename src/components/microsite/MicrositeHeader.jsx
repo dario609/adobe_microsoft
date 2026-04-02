5cb297f029ee018abf23a98574bd0b7443af92e0
amministrazionemfce <amministrazionemfce@gmail.com>
2026-04-02 16:04:22 -0600

    test

--- src/components/microsite/MicrositeHeader.jsx
+++ src/components/microsite/MicrositeHeader.jsx
@@ -1,12 +1,10 @@
+import { BRAND_NAME } from '../../constants/config.js'
 import { ExperienceLogoImg } from './ExperienceLogoImg.jsx'
 
 export function MicrositeHeader() {
   return (
-    <header className="landHeader">
+    <header className="landHeader" aria-label={BRAND_NAME}>
       <ExperienceLogoImg className="landHeader__logo" width={48} height={48} />
-      <div className="landHeader__text">
-        <h1 className="landHeader__title">Express-Kiosk</h1>
-      </div>
     </header>
   )
 }
